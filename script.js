d20ab10dade2387052993dab3b4c4d8764cb3acc
Rafal <raf@brandsmithz.com>
2021-05-02 13:35:16 +0100

    quick update to self invoke fb

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ app.listen('3000', () => {
 })
 
 app.use(express.static('public'))
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAKattZxSVNCvHhwXstBQVtFxH6zlcr5ao",
+    authDomain: "vanillajsauth-76afb.firebaseapp.com",
+    projectId: "vanillajsauth-76afb",
+    storageBucket: "vanillajsauth-76afb.appspot.com",
+    messagingSenderId: "804353047242",
+    appId: "1:804353047242:web:7a80f327fb8140b3211233",
+    measurementId: "G-LQ8DP7S3RF"
+  };
+  
+
+app.get('/config',(request, response) => {
+response.json(firebaseConfig)
+})
